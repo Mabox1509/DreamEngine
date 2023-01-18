be3716498a04aa16b9cab68794a28e7bc2b9580b
Mabox1509 <73403310+Mabox1509@users.noreply.github.com>
2023-01-17 21:54:49 -0600

    17/01/2021

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DreamEngine
-{
-    public class Class1
-    {
-    }
-}
---
+++ Core.cs
@@ -0,0 +1,66 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using System;
+
+namespace DreamEngine
+{
+    public class Core
+    {
+        #region VARIABLES
+        //Window
+        private GameWindow window;
+
+        #endregion
+
+        #region METHODS
+        public Core(Vector2i size ,int scale = 1, int fps = 60,string title = "My Game") 
+        {
+            //Set settings
+            GameWindowSettings windowSettings = new GameWindowSettings();
+            windowSettings.RenderFrequency = fps;
+            windowSettings.UpdateFrequency = fps;
+
+
+            NativeWindowSettings nativeSettings = new NativeWindowSettings();
+            nativeSettings.Size = size * scale;
+            nativeSettings.MaximumSize = size * scale;
+            nativeSettings.MinimumSize = size * scale;
+            nativeSettings.Title = title;
+
+
+            window = new GameWindow(windowSettings, nativeSettings);
+            window.CenterWindow();
+
+            window.Load += Window_Load;
+            window.Unload += Window_Unload;
+
+            window.UpdateFrame += Window_UpdateFrame;
+            window.RenderFrame += Window_RenderFrame;
+        }
+
+        
+
+        private void Window_Load()
+        {
+
+        }
+        private void Window_Unload()
+        {
+
+        }
+
+
+        
+
+        private void Window_UpdateFrame(OpenTK.Windowing.Common.FrameEventArgs obj)
+        {
+
+        }
+        private void Window_RenderFrame(OpenTK.Windowing.Common.FrameEventArgs obj)
+        {
+
+        }
+
+        #endregion
+    }
+}
